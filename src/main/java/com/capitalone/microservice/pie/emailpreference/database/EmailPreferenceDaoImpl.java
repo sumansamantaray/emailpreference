0a3e4a6b6ec813249acd434f5aa77a87e27a28f9
sumansamantaray <suman.samantaray@gmail.com>
2018-07-06 17:02:53 -0500

    Updated for H2 DB insert call

--- src/main/java/com/capitalone/microservice/pie/emailpreference/database/EmailPreferenceDaoImpl.java
+++ src/main/java/com/capitalone/microservice/pie/emailpreference/database/EmailPreferenceDaoImpl.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.capitalone.microservice.pie.emailpreference.database.model.EmailPreferenceObject;
+import com.capitalone.microservice.pie.emailpreference.database.model.EmailPreferences;
 
 /**
  * @author Suman
@@ -22,21 +22,24 @@ public class EmailPreferenceDaoImpl implements EmailPreferenceDao {
 	
 	@Override
 	@Bean
-	public int insertEmailPreference(EmailPreferenceObject emailPrefObj) {
-		piecofJdbcTemplate.update("insert into email_preferences (account_number, email_address, domesticInb, domesticOut, internationalInb, internationalOut, bookInb, bookOut, email_format, status) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", emailPrefObj);
+	public int insertEmailPreference(EmailPreferences emailPrefObj) {
+		piecofJdbcTemplate.update("insert into email_preferences (account_number, email_address, domestic_Inb, domestic_Out, international_Inb, international_Out, book_Inb, book_Out, email_format, status) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+				new Object[] {emailPrefObj.getAccountNumber(), emailPrefObj.getEmailAddress(), emailPrefObj.getDomesticInb(),
+						emailPrefObj.getDomesticOut(), emailPrefObj.getInternationalInb(), emailPrefObj.getInternationalOut(),
+						emailPrefObj.getBookInd(), emailPrefObj.getBookOut(), emailPrefObj.getEmailFormat(), emailPrefObj.getStatus()});
 		return 0;
 	}
 
 	@Override
 	@Bean
-	public int updateEmailPreference(EmailPreferenceObject emailPrefObj) {
+	public int updateEmailPreference(EmailPreferences emailPrefObj) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	@Bean
-	public int deleteEmailPreference(EmailPreferenceObject emailPrefObj) {
+	public int deleteEmailPreference(EmailPreferences emailPrefObj) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
